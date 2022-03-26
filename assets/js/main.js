6a04fdf892517d2c0714f9c5df5228c99509b73b
jester <jaimebuddy98@gmail.com>
2022-03-25 22:04:42 -0500

    edit js to taste

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,10 @@
   /* selector helper function */
   const select = (el, all = false) => {
     el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
+    if (all) {return [...document.querySelectorAll(el)]} 
+    else {return document.querySelector(el)}
   }
+
 
   /* event listener function */
   const on = (type, el, listener, all = false) => {
@@ -24,10 +22,13 @@
     }
   }
 
+
   /* on scroll event listener */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
+
+  /* ------------------------------------------------------------------------------ */
 
   /* Navbar links light up while scroll */
   let navbarlinks = select('#navbar .scrollto', true)
@@ -36,7 +37,9 @@
     navbarlinks.forEach(navbarlink => {
       if (!navbarlink.hash) return
       let section = select(navbarlink.hash)
+
       if (!section) return
+
       if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
         navbarlink.classList.add('active')
       } else {
@@ -47,6 +50,7 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
+
   /* Scrolls to an element with header offset */
   const scrollto = (el) => {
     let elementPos = select(el).offsetTop
@@ -56,12 +60,14 @@
     })
   }
 
+
   /* Mobile nav toggle */
   on('click', '.mobile-nav-toggle', function(e) {
-    select('body').classList.toggle('mobile-nav-active')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
+      select('body').classList.toggle('mobile-nav-active')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
   })
+
 
   /* Scroll with ofset on links with a class name .scrollto */
   on('click', '.scrollto', function(e) {
@@ -70,14 +76,15 @@
 
       let body = select('body')
       if (body.classList.contains('mobile-nav-active')) {
-        body.classList.remove('mobile-nav-active')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+          body.classList.remove('mobile-nav-active')
+          let navbarToggle = select('.mobile-nav-toggle')
+          navbarToggle.classList.toggle('bi-list')
+          navbarToggle.classList.toggle('bi-x')
       }
       scrollto(this.hash)
     }
   }, true)
+
 
   /* Skills animation */
   let skilsContent = select('.skills-content');
@@ -85,8 +92,10 @@
     new Waypoint({
       element: skilsContent,
       offset: '80%',
+
       handler: function(direction) {
         let progress = select('.progress .progress-bar', true);
+        
         progress.forEach((el) => {
           el.style.width = el.getAttribute('aria-valuenow') + '%'
         });
